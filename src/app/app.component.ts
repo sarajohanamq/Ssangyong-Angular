8e1ce258ec2f44cfd643c188e9751b2e8d4b4bea
Sara <sjohanamantilla@ucundinamarca.edu.co>
2021-06-11 07:59:15 -0500

    retoques finales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Routes, Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ssangyong';
-  constructor() { }
+  
+  constructor(private router:Router) { }
+
 }
